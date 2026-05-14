2d1d6cd00a48b6cb81f0e68424118a56fd48f7a2
Abhishek-Gajbhare <abhishekgajbhare114@gmail.com>
2026-05-14 17:46:05 +0530

    changed mysql to sqlite

--- Examproject/settings.py
+++ Examproject/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'Examproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'onlinetestportal1311db',
-        'USER': 'root',
-        'PASSWORD': 'mysqlroot',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
