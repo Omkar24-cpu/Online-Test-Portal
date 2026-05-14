57d1e750305b04c89b960cb16da69f644a4d985a
Abhishek-Gajbhare <abhishekgajbhare114@gmail.com>
2026-05-14 17:36:37 +0530

    fixed allowed hosts

--- Examproject/settings.py
+++ Examproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dna9lm8e3wo1q4q6@8h7&+trr4y_)^^a#4x8li_!g(_rcwe$i+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
